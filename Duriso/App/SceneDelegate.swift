ca4d53e44da2f161ed9f6c9bb927f1f91822a22a
DongHyeonDev <dhkim960206@gmail.com>
2025-08-29 19:41:27 +0900

    Fix: 네트워크 모니터링 코드 중복으로 인한 코드 제거 (SceneDelegate.swift)
    - AppDelegate, SceneDelegate 중복으로 인해 Application Life Cycle에 따라 앱이 시작되고 완전히 종료될 때 적용하는 것이 맞다고 판단함.

--- Duriso/App/SceneDelegate.swift
+++ Duriso/App/SceneDelegate.swift
@@ -18,9 +18,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     // 스토리보드를 사용하는 경우 `window` 프로퍼티가 자동으로 초기화되어 씬에 첨부됩니다.
     // 이 델리게이트는 연결 장면이나 세션이 새로운 것을 의미하지 않습니다(대신 `application:configurationForConnectingSceneSession` 참조).
     
-    // 네트워크 모니터링 시작
-    NetworkMonitor.shared.startMonitoring()
-    
     // scene이 UIWindowScene으로 캐스팅되지 않으면 함수 종료
     guard let windowScene = (scene as? UIWindowScene) else { return }
     
@@ -55,8 +52,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     // 다음에 scene이 연결될 때 다시 생성할 수 있는 이 scene과 관련된 모든 리소스를 해제합니다.
     // 해당 세션이 반드시 삭제된 것은 아니므로 나중에 장면이 다시 연결될 수 있습니다(대신 `application:didDiscardSceneSessions` 참조).
     
-    /// 네트워크 모니터링 중지
-    NetworkMonitor.shared.stopMonitoring()
     print("장면이 분리되었습니다.")
   }
 
