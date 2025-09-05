6e58c5414b7c576788b0e7439f0ee78d157c3bad
DongHyeonDev <dhkim960206@gmail.com>
2025-09-05 23:05:10 +0900

    Comment: UIImage+Resize 메서드 설명 주석 추가

--- Duriso/Extensions/UIImage+Resize.swift
+++ Duriso/Extensions/UIImage+Resize.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 extension UIImage {
+  /// CGSize 값을 통한 UIImage Resize Extension
+  /// - Parameter size: Resize 하려는 CGSize 값
+  /// - Returns: Resize 된 이미지
   func resized(to size: CGSize) -> UIImage? {
     UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
     defer { UIGraphicsEndImageContext() }
@@ -16,6 +19,10 @@ extension UIImage {
     
     return UIGraphicsGetImageFromCurrentImageContext()
   }
+  
+  /// Percent 값을 통한 UIImage Resize Extension
+  /// - Parameter percentage: Resize 하려는 percent 값
+  /// - Returns: Resize 된 이미지
   func resized(withPercentage percentage: CGFloat) -> UIImage? {
     let canvasSize = CGSize(width: size.width * percentage, height: size.height * percentage)
     UIGraphicsBeginImageContextWithOptions(canvasSize, false, scale)
@@ -23,6 +30,10 @@ extension UIImage {
     draw(in: CGRect(origin: .zero, size: canvasSize))
     return UIGraphicsGetImageFromCurrentImageContext()
   }
+  
+  /// Width 값을 통한 UIImage Resize Extension
+  /// - Parameter width: Resize 하려는 width 값
+  /// - Returns: Resize 된 이미지
   func resized(toWidth width: CGFloat) -> UIImage? {
     let canvasSize = CGSize(width: width, height: CGFloat(ceil(width/size.width * size.height)))
     UIGraphicsBeginImageContextWithOptions(canvasSize, false, scale)
